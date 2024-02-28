3526cadc4899c3fc595f976cd01936d0f5846b71
Delta22z <100379615+Delta22z@users.noreply.github.com>
2024-02-28 11:59:11 -0700

    3

--- manifest.js
+++
@@ -1,9 +0,0 @@
-fetch('manifest.json')
-  .then(response => response.json())
-  .then(data => {
-    // Trabaja con los datos del manifiesto (data)
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error al cargar el archivo de manifiesto:', error);
-  });
